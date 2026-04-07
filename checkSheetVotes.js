2e608d432560bd16e52bb6a141fb08c6bd0a6819
uko05 <yosieyosie1@yahoo.co.jp>
2026-04-07 23:32:00 +0900

    Initial commit

--- checkSheetVotes.js
+++ checkSheetVotes.js
@@ -6,9 +6,25 @@ import {
   serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
 
+const QUESTION_LABELS = {
+  'TAB-01': 'TAB-01_始めた時期は？',
+  'TAB-02': 'TAB-02_好きなバージョンは？',
+  'TAB-03': 'TAB-03_推しキャラは？',
+  'TAB-04': 'TAB-04_髪型が似てるキャラは？',
+  'TAB-05': 'TAB-05_親友にするなら？',
+  'TAB-06': 'TAB-06_兄妹にするなら？',
+  'TAB-07': 'TAB-07_結婚するなら？',
+  'TAB-08': 'TAB-08_同じ声になれるなら？',
+  'TAB-09': 'TAB-09_１日入れ替わるなら？',
+  'TAB-10': 'TAB-10_着てみたいキャラの服装は？',
+  'TAB-11': 'TAB-11_最初に引いた星５は？',
+  'TAB-12': 'TAB-12_一番欲しいキャラは？',
+};
+
 // checkSheetVotes/{questionId}/characters/{characterFile} = { count, updatedAt }
 async function incrementVote(questionId, characterFile) {
-  const ref = doc(db, "checkSheetVotes", questionId, "characters", characterFile);
+  const docId = QUESTION_LABELS[questionId] ?? questionId;
+  const ref = doc(db, "checkSheetVotes", docId, "characters", characterFile);
   await runTransaction(db, async (tx) => {
     const snap = await tx.get(ref);
     if (!snap.exists()) {
